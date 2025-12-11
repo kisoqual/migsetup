1aac02c4c01a5e1cf02857fd6fe601647ecd8ddc
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-12-12 00:23:18 +0300

    Add files via upload

---
+++ migsetup.ps1
@@ -0,0 +1,20 @@
+﻿$migVarName;
+$choice = Read-Host "Установить EntityFramework перед миграцией? (Y/N)";
+    
+function TurnMigratiON {
+    param ($migParamName)
+    Enable-Migrations;
+    $migParamName = Read-Host "Введите имя переменной для миграции: ";
+    Add-Migration $migParamName;
+    Update-DataBase;
+}
+
+if ($choice -eq "y") {
+    Install-Package EntityFramework;
+    TurnMigratiON($migVarName);
+    Pause;
+}
+else {
+    TurnMigratiON($migVarName);
+    Pause;
+}
