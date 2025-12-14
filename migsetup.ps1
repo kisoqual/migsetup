81595eeff9537723f9966efdd4b9943c7dcaa4a5
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-12-14 18:53:05 +0300

    Update migsetup.ps1

--- migsetup.ps1
+++ migsetup.ps1
@@ -1,20 +1,50 @@
-﻿$migVarName;
-$choice = Read-Host "Install EntityFramework before migration? (Y/N)";
+$migVarName;
+$choice = Read-Host "Установить EntityFramework перед миграцией? (Y/N)";
     
+function Menu {
+    Write-Output "`n----Мастер миграции Code First в EF----";
+    Write-Output "1. Включение миграции`n2. Добавление миграции";
+    $choice = Read-Host;
+    if ($choice -eq "1") {
+        TurnMigratiON($migVarName);
+        Pause;
+        exit;
+    }
+    if ($choice -eq "2") {
+        AddMigration($migVarName);
+        Pause;
+        exit;
+    }
+    if ($choice -eq "3") {
+        Pause;
+        exit;
+    }
+    else {
+        cls;
+        Write-Output "Неверный ввод!";
+        Menu;
+    }
+}  
+
 function TurnMigratiON {
     param ($migParamName)
     Enable-Migrations;
-    $migParamName = Read-Host "Enter variable name for migration: ";
+    $migParamName = Read-Host "Введите имя переменной для миграции: ";
     Add-Migration $migParamName;
+    Update-DataBase;
+}
+
+function AddMigration {
+    param ($migInnerVarName)
+    $migInnerVarName = Read-Host "Введите имя переменной для миграции: ";
+    Add-Migration $migInnerVarName;
     Update-DataBase;
 }
 
 if ($choice -eq "y") {
     Install-Package EntityFramework;
-    TurnMigratiON($migVarName);
-    Pause;
+    Menu;
 }
 else {
-    TurnMigratiON($migVarName);
-    Pause;
+    Menu;
 }
